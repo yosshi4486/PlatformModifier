586c602f1140b3ce3abba706edf6e7eba70793eb
yosshi4486 <yoshiharuseijin@gmail.com>
2020-07-07 16:53:08 +0900

    Removes unused test is added by Xcode.

--- Tests/PlatformModifierTests/PlatformModifierTests.swift
+++ Tests/PlatformModifierTests/PlatformModifierTests.swift
@@ -6,7 +6,7 @@ final class PlatformModifierTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PlatformModifier().text, "Hello, World!")
+//        XCTAssertEqual(PlatformModifier().text, "Hello, World!")
     }
 
     static var allTests = [
