e55750a9d3cfaafd059b1e43b199fa049f0c69d6
yosshi4486 <yoshiharuseijin@gmail.com>
2020-07-07 16:53:44 +0900

    Implements platform modifiers.

--- Sources/PlatformModifier/PlatformModifier.swift
+++ Sources/PlatformModifier/PlatformModifier.swift
@@ -1,3 +1,53 @@
-struct PlatformModifier {
-    var text = "Hello, World!"
+import SwiftUI
+
+extension View {
+    
+    /// Modifies upstream content only when the platform is iOS.
+    /// - Parameter modifier: A modifier.
+    /// - Returns: A modified view.
+    func iOS<Content : View>(_ modifier: (Self) -> Content) -> some View {
+        
+        #if os(iOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+    
+    /// Modifies upstream content only when the platform is macOS.
+    /// - Parameter modifier: A modifier.
+    /// - Returns: A modified view.
+    func macOS<Content : View>(_ modifier: (Self) -> Content) -> some View {
+        
+        #if os(macOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+    
+    /// Modifies upstream content only when the platform is tvOS.
+    /// - Parameter modifier: A modifier.
+    /// - Returns: A modified view.
+    func tvOS<Content : View>(_ modifier: (Self) -> Content) -> some View {
+        
+        #if os(tvOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+
+    /// Modifies upstream content only when the platform is watchOS.
+    /// - Parameter modifier: A modifier.
+    /// - Returns: A modified view.
+    func watchOS<Content : View>(_ modifier: (Self) -> Content) -> some View {
+        
+        #if os(watchOS)
+        return modifier(self)
+        #else
+        return self
+        #endif
+    }
+
 }
