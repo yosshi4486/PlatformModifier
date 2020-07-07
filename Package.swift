acdc28747b2dff74be410739029a6eb03c40189b
yosshi4486 <yoshiharuseijin@gmail.com>
2020-07-07 15:54:18 +0900

    Specifies target platforms.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "PlatformModifier",
+    platforms: [.iOS(.v14), .macOS(.v10_16), .watchOS(.v7), .tvOS(.v14)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
